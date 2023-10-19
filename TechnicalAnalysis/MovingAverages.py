539a1095148f2f2949a5d9615c3f909b7a9feaa6
msg8924 <45294471+msg8924@users.noreply.github.com>
2023-10-19 13:08:08 -0400

    Added Moving Averages Signal

---
+++ TechnicalAnalysis/MovingAverages.py
@@ -0,0 +1,36 @@
+from quantfinance.Utils import Utilities as utils
+import datetime
+import matplotlib.pyplot as plt
+
+def compute_signals(data, stock_name, short_period=50, long_period=200):
+    data['Short SMA'] = data[stock_name].rolling(window=short_period).mean()
+    data['Long SMA'] = data[stock_name].rolling(window=long_period).mean()
+    data['Short EMA'] = data[stock_name].ewm(span=short_period,adjust=False).mean()
+    data['Long EMA'] = data[stock_name].ewm(span=long_period, adjust=False).mean()
+    return data.dropna()
+
+def plot_moving_averages(data,stock_ticker):
+    plt.figure(figsize=(12,6))
+    plt.plot(data[stock_ticker],label='Stock Price', color='Black')
+    plt.plot(data['Short SMA'], label='Short SMA', color='Red')
+    plt.plot(data['Long SMA'], label='Long SMA', color='Green')
+    plt.plot(data['Short EMA'], label='Short EMA', color='Orange')
+    plt.plot(data['Long EMA'], label='Long EMA', color='Blue')
+    plt.xlabel("Time")
+    plt.ylabel("Price")
+    plt.title("Moving Averages (MA) Indicators")
+    plt.show()
+
+if __name__ == "__main__":
+    start = datetime.datetime(2022,1,1)
+    end = datetime.datetime(2023,10,1)
+    ticker = ['JPM']
+    data = utils.download_data(ticker, start, end)
+    print(data)
+    data = compute_signals(data, 'JPM')
+    print(data)
+    plot_moving_averages(data, 'JPM')
+
+
+
+
